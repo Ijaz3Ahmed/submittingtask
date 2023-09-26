ef7bda9f25304dfdece80ac9b97691ead8d377b4
Ijaz3Ahmed <146079789+Ijaz3Ahmed@users.noreply.github.com>
2023-09-26 17:07:32 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+var result=0;
+
+function decrease()
+{
+     result=result-5;
+    document.getElementById("resultlabel").textContent=result;
+}
+function increase()
+{
+    result=result+5;
+    document.getElementById("resultlabel").textContent=result;
+    
+}
